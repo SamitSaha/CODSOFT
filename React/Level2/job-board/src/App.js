1497f3f161002ccd0a46fd93aef8c7b426ed4ab8
Samit Saha <samitsaha99@gmail.com>
2025-03-28 22:26:47 +0600

    modified

--- React/Level2/job-board/src/App.js
+++ React/Level2/job-board/src/App.js
@@ -4,31 +4,30 @@ import Home from "./pages/Home";
 import JobListings from "./pages/JobListings";
 import JobDetail from "./pages/JobDetail";
 import EmployerDashboard from "./pages/EmployerDashboard";
-// import CandidateDashboard from "./pages/CandidateDashboard";
+
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Register from "./pages/Register";
 import Navbar from "./components/Navbar";
-// import Footer from "./components/Footer";
 
-function App() {
+const App = () => {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/jobs" element={<JobListings />} />
-        <Route path="/jobs/:id" element={<JobDetail />} />
-        <Route path="/employer-dashboard" element={<EmployerDashboard />} />
-        {/* <Route path="/candidate-dashboard" element={<CandidateDashboard />} />  */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-      {/* <Footer /> */}      
-    </Router>
-    
-  );
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/jobs" element={<JobListings />} />
+          <Route path="/jobs/:id" element={<JobDetail />} />
+          <Route path="/employer-dashboard" element={<EmployerDashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+        {/* <Footer /> */}
+      </Router>
+    </>
+  )
 }
 
 export default App;
--- React/Level2/online-quiz-maker/src/Components/Quiz/Quiz.js
+++ React/Level2/online-quiz-maker/src/Components/Quiz/Quiz.js
@@ -1,88 +1,88 @@
-import React, { useRef, useState } from 'react'
-import './Quiz.css'
+import React, { useRef, useState } from 'react';
+import './Quiz.css';
 import { data } from '../../assests/data';
 
 const Quiz = () => {
-  let [index, setIndex] = useState(0);
-  let [question, setQuestion] = useState(data[index] || {});
-  let [lock, setLock] = useState(false);
-  let [score, setScore] = useState(0);
-  let [result, setResult] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(data[currentIndex] || {});
+  const [isLocked, setIsLocked] = useState(false);
+  const [totalScore, setTotalScore] = useState(0);
+  const [isCompleted, setIsCompleted] = useState(false);
 
-  let Option1 = useRef(null);
-  let Option2 = useRef(null);
-  let Option3 = useRef(null);
-  let Option4 = useRef(null);
+  const refOption1 = useRef(null);
+  const refOption2 = useRef(null);
+  const refOption3 = useRef(null);
+  const refOption4 = useRef(null);
 
-  let option_array = [Option1, Option2, Option3, Option4];
+  const optionsArray = [refOption1, refOption2, refOption3, refOption4];
 
-  const checkAns = (e, ans) => {
-    if (lock === false) {
-      if (question.ans === ans) {
-        e.target.classList.add("correct");
-        setLock(true);
-        setScore(prev => prev + 1);
-      }
-      else {
-        e.target.classList.add("wrong");
-        setLock(true);
-        // option_array[question.ans-1].current.classList.add("correct");
-        if (option_array[question.ans - 1]?.current) {
-          option_array[question.ans - 1].current.classList.add("correct");
+  const handleAnswerSelection = (event, selectedAnswer) => {
+    if (!isLocked) {
+      if (currentQuestion.ans === selectedAnswer) {
+        event.target.classList.add("correct");
+        setTotalScore(prevScore => prevScore + 1);
+      } else {
+        event.target.classList.add("wrong");
+        if (optionsArray[currentQuestion.ans - 1]?.current) {
+          optionsArray[currentQuestion.ans - 1].current.classList.add("correct");
         }
       }
+      setIsLocked(true);
     }
-  }
+  };
 
-  const next = () => {
-    if (lock === true) {
-      if (index === data.length - 1) {
-        setResult(true);
-        return 0;
+  const moveToNextQuestion = () => {
+    if (isLocked) {
+      if (currentIndex === data.length - 1) {
+        setIsCompleted(true);
+        return;
       }
-      setIndex(++index);
-      setQuestion(data[index]);
-      setLock(false);
-      option_array.map((option) => {
-        option.current.classList.remove("wrong");
-        option.current.classList.remove("correct");
-        return null;
-      })
+      setCurrentIndex(prevIndex => prevIndex + 1);
+      setCurrentQuestion(data[currentIndex + 1]);
+      setIsLocked(false);
+      optionsArray.forEach(option => {
+        if (option.current) {
+          option.current.classList.remove("wrong", "correct");
+        }
+      });
     }
-  }
+  };
 
-  const reset = () => {
-    setIndex(0);
-    setQuestion(data[0]);
-    setScore(0);
-    setLock(false);
-    setResult(false);
-  }
+  const restartQuiz = () => {
+    setCurrentIndex(0);
+    setCurrentQuestion(data[0]);
+    setTotalScore(0);
+    setIsLocked(false);
+    setIsCompleted(false);
+  };
 
   return (
     <div className='container'>
       <h1>Quiz App</h1>
       <hr />
-      {result ? <></> : <>
-        <h2>{index + 1}. {question.questions}</h2>
-        <ul>
-          <li ref={Option1} onClick={(e) => { checkAns(e, 1) }} >{question.option1}</li>
-          <li ref={Option2} onClick={(e) => { checkAns(e, 2) }} >{question.option2}</li>
-          <li ref={Option3} onClick={(e) => { checkAns(e, 3) }} >{question.option3}</li>
-          <li ref={Option4} onClick={(e) => { checkAns(e, 4) }} >{question.option4}</li>
-        </ul>
-        <button onClick={next} type='button'>Next</button>
+      {!isCompleted ? (
+        <>
+          <h2>{currentIndex + 1}. {currentQuestion.questions}</h2>
+          <ul>
+            <li ref={refOption1} onClick={(e) => handleAnswerSelection(e, 1)}>{currentQuestion.option1}</li>
+            <li ref={refOption2} onClick={(e) => handleAnswerSelection(e, 2)}>{currentQuestion.option2}</li>
+            <li ref={refOption3} onClick={(e) => handleAnswerSelection(e, 3)}>{currentQuestion.option3}</li>
+            <li ref={refOption4} onClick={(e) => handleAnswerSelection(e, 4)}>{currentQuestion.option4}</li>
+          </ul>
+          <button onClick={moveToNextQuestion} type='button'>Next</button>
 
-        <div className="index">
-          {index + 1} of {data.length} questions
-        </div>
-      </>}
-      {result ? <>
-        <h2>Your score is {score} out of {data.length}.</h2>
-        <button onClick={reset}>Reset</button>
-      </> : <></>}
+          <div className="index">
+            {currentIndex + 1} of {data.length} questions
+          </div>
+        </>
+      ) : (
+        <>
+          <h2>Your score is {totalScore} out of {data.length}.</h2>
+          <button onClick={restartQuiz}>Reset</button>
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Quiz
+export default Quiz;
